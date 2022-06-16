b7b32e23084eff9f2c267067cb8d09afc53740e5
NVMKhang <86240214+NVMKhang@users.noreply.github.com>
2022-06-16 16:37:42 +0700

    Update fbConfig.ts

--- src/api/fbConfig.ts
+++ src/api/fbConfig.ts
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC0qczUV5IZxtrsdh1rP0A79tklpGUtfqs",
